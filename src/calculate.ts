4dc0ae725c8343562dac8816da8a4fbd9987408e
Matt Congdon <mattcongdon95@gmail.com>
2022-04-07 14:36:15 +0100

    calculate english base 6 & pythagorean

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,8 +1,30 @@
-const calculateSimple = (s: string): number => {
+export const calculateSimple = (s: string): number => {
     return [...s.toUpperCase()]
-    .map(e => e.charCodeAt(0) - 64)
-    .filter(x => x > 0 && x < 27)
-    .reduce((acc, i) => acc + i)
+        .map(e => e.charCodeAt(0) - 64)
+        .filter(x => x > 0 && x < 27)
+        .reduce((acc, i) => acc + i)
 }
 
-export default calculateSimple
+export const calculateBase6 = (s: string): number => {
+    return [...s.toUpperCase()]
+        .map(e => (e.charCodeAt(0) - 64) * 6)
+        .filter(x => x > 5 && x < 157)
+        .reduce((acc, i) => acc + i)
+}
+
+export const calculatePythagorean = (s: string): number => {
+    const arr1: number[] = [...s.toUpperCase()]
+        .map(e => e.charCodeAt(0) - 64)
+        .filter(x => x > 0 && x < 27)
+    const arr2: number[] = []
+    arr1.forEach(e => {
+        if (e > 9 && e <= 18) {
+            arr2.push(e - 9)
+        } else if (e >= 19 && e < 27) {
+            arr2.push(e - 18)
+        } else {
+            arr2.push(e)
+        }
+    })
+    return arr2.reduce((acc, i) => acc + i)
+}
--- src/tests/calculate.test.ts
+++ src/tests/calculate.test.ts
@@ -1,5 +1,13 @@
-import calculateSimple from '../calculate'
+import { calculateSimple, calculateBase6, calculatePythagorean } from '../calculate'
 
 test("calculate simple value", () => {
     expect(calculateSimple("abcde")).toBe(15)
+})
+
+test("calculate base 6 value", () => {
+    expect(calculateBase6("abcde")).toBe(90)
+})
+
+test("calculate pythagorean value", () => {
+    expect(calculatePythagorean("zxcvb")).toBe(23)
 })
