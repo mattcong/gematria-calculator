f76144f3de9fd7a371551bbf2468040f034f6bf7
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2022-05-08 19:48:08 +0100

    general calculation function

--- src/tests/calculate.test.ts
+++ src/tests/calculate.test.ts
@@ -1,13 +1,19 @@
-import { calculateSimple, calculateBase6, calculatePythagorean } from '../calculate'
+import { calculate } from '../calculate'
+import { simpleAlphabet, base6Alphabet, pythagoreanAlphabet, hebrewAlphabet } from "../ciphers"
+
 
 test("calculate simple value", () => {
-    expect(calculateSimple("abcde")).toBe(15)
+    expect(calculate("abcde", simpleAlphabet.use())).toStrictEqual(15)
 })
 
 test("calculate base 6 value", () => {
-    expect(calculateBase6("abcde")).toBe(90)
+    expect(calculate("abcde", base6Alphabet.use())).toStrictEqual(90)
 })
 
 test("calculate pythagorean value", () => {
-    expect(calculatePythagorean("zxcvb")).toBe(23)
+    expect(calculate("abjsz", pythagoreanAlphabet.use())).toStrictEqual(13)
+})
+
+test("calculate hebrew value", () => {
+    expect(calculate("abjsz", hebrewAlphabet.use())).toStrictEqual(913)
 })
