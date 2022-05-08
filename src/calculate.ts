f2d75f8919b9f31525c795daacb2d1758afb31cb
Matt Congdon <mattcongdon95@gmail.com>
2022-05-08 19:48:08 +0100

    general calculation function

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,30 +1,13 @@
-export const calculateSimple = (s: string): number => {
-    return [...s.toUpperCase()]
-        .map(e => e.charCodeAt(0) - 64)
-        .filter(x => x > 0 && x < 27)
-        .reduce((acc, i) => acc + i)
-}
+import { Alphabet } from "./alphabets"
 
-export const calculateBase6 = (s: string): number => {
-    return [...s.toUpperCase()]
-        .map(e => (e.charCodeAt(0) - 64) * 6)
-        .filter(x => x > 5 && x < 157)
-        .reduce((acc, i) => acc + i)
-}
 
-export const calculatePythagorean = (s: string): number => {
-    const arr1: number[] = [...s.toUpperCase()]
-        .map(e => e.charCodeAt(0) - 64)
-        .filter(x => x > 0 && x < 27)
-    const arr2: number[] = []
-    arr1.forEach(e => {
-        if (e > 9 && e <= 18) {
-            arr2.push(e - 9)
-        } else if (e >= 19 && e < 27) {
-            arr2.push(e - 18)
-        } else {
-            arr2.push(e)
-        }
-    })
-    return arr2.reduce((acc, i) => acc + i)
+export function calculate(s: string, alphabet: Alphabet): number {
+    let result: number[] = []
+
+    for (const letter in alphabet) {
+        [...s.toUpperCase()].forEach(e => {
+            if (letter === e) { result.push(alphabet[letter]) }
+        })
+    }
+    return result.reduce((a, b) => a + b)
 }
