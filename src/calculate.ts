94ca83b525a66c92b55df2b4c773f8d84e80da8e
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2022-05-09 16:40:40 +0100

    find shared words from txt file

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,9 +1,9 @@
+import * as fs from 'fs';
 import { Alphabet } from "./alphabets"
 
 
 export function calculate(s: string, alphabet: Alphabet): number {
     let result: number[] = []
-
     for (const letter in alphabet) {
         [...s.toUpperCase()].forEach(e => {
             if (letter === e) { result.push(alphabet[letter]) }
@@ -11,3 +11,11 @@ export function calculate(s: string, alphabet: Alphabet): number {
     }
     return result.reduce((a, b) => a + b)
 }
+
+export function findShared(words: string[], value: number, alphabet: Alphabet) {
+    const sharedWords: string[] = []
+    words.forEach(e => {if (value === calculate(e, alphabet)) { sharedWords.push(e) }}) 
+    return sharedWords.toString().replaceAll(',', ', ')
+}
+
+export const wordList = fs.readFileSync('./public/british-english.txt', 'utf-8').toString().split(' \n')
