6856cb8050184a6037353c8230f0e5ffb84cf68d
Matt Congdon <mattcongdon95@gmail.com>
2022-05-09 17:11:06 +0100

    views

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { index } from './routes/index';
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
