9ac3d686015f5d8844e3483add29b02e3230a3b5
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2022-05-16 10:21:32 +0100

    reverse ciphers

--- src/tests/calculate.test.ts
+++ src/tests/calculate.test.ts
@@ -1,19 +1,27 @@
 import { calculate } from '../modules/calculate'
-import { simpleAlphabet, base6Alphabet, pythagoreanAlphabet, hebrewAlphabet } from "../modules/ciphers"
+import { simpleAlphabet, standardAlphabet, reverseStandardAlphabet, pythagoreanAlphabet, reversePythagoreanAlphabet, multiple6Alphabet } from "../modules/ciphers"
 
 
 test("calculate simple value", () => {
     expect(calculate("abcde", simpleAlphabet.use())).toStrictEqual(15)
 })
 
-test("calculate base 6 value", () => {
-    expect(calculate("abcde", base6Alphabet.use())).toStrictEqual(90)
+test("calculate standard value", () => {
+    expect(calculate("abjsz", standardAlphabet.use())).toStrictEqual(913)
+})
+
+test("calculate reverse standard value", () => {
+    expect(calculate("abjsz", reverseStandardAlphabet.use())).toStrictEqual(1589)
 })
 
 test("calculate pythagorean value", () => {
     expect(calculate("abjsz", pythagoreanAlphabet.use())).toStrictEqual(13)
 })
 
-test("calculate hebrew value", () => {
-    expect(calculate("abjsz", hebrewAlphabet.use())).toStrictEqual(913)
+test("calculate reverse pythagorean value", () => {
+    expect(calculate("abjsz", reversePythagoreanAlphabet.use())).toStrictEqual(32)
+})
+
+test("calculate multiple 6 value", () => {
+    expect(calculate("abcde", multiple6Alphabet.use())).toStrictEqual(90)
 })
