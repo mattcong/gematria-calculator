ab6df29b933ea059b3b1a15c765d2bda9ced7604
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2024-02-24 15:04:01 +0000

    add info pages

---
+++ src/app/information/ciphers/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Page = () => {
+  return (
+    <>
+      <Link href="/" className="button">
+        Back
+      </Link>
+    </>
+  )
+}
+
+export default Page
