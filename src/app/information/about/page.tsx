6dfc09c5d3c89843c456c8d3ed6e621780ff4718
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2024-02-26 10:33:57 +0000

    app bar, image sizing

--- src/app/information/about/page.tsx
+++ src/app/information/about/page.tsx
@@ -1,14 +1,20 @@
 import { Philosophorum } from "@/components/svg/Philosophorum"
+import AppBar from "@/components/AppBar"
 import Link from "next/link"
 
 const Page = () => {
   return (
     <div className="container">
-      <Link href="/" className="button">
-        Back
-      </Link>
+      <AppBar>
+        <Link href="/" className="button">
+          Back
+        </Link>
+      </AppBar>
       <div className="information-container">
         <Philosophorum />
+        <div className="information-wrap">
+          <p></p>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,5 @@
+const AppBar = ({ children }: { children: React.ReactNode }) => {
+  return <div className="app-bar">{children}</div>
+}
+
+export default AppBar
