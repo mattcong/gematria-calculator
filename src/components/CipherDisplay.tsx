bd1f1c41353a32bfcbc732d0553b2a2a4079e88b
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2024-03-30 22:22:45 +0000

    cipher display component

---
+++ src/components/CipherDisplay.tsx
@@ -0,0 +1,21 @@
+import { calculate } from "@/lib/calculate"
+import { Alphabet } from "@/types/Alphabet"
+
+const CipherDisplay = ({ letters, cipher }: { letters: string[]; cipher: Alphabet }) => {
+  return (
+    <div className="cipher-display-wrap">
+      {letters.map((letter) => (
+        <div className="cipher-display-tile-wrap" key={letter}>
+          <div className="cipher-display-tile">
+            <p style={{ margin: 0 }}>{letter}</p>
+          </div>
+          <div className="cipher-display-tile">
+            <p style={{ margin: 0 }}>{calculate(letter, cipher)}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default CipherDisplay
