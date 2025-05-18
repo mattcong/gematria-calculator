f6f1ea943fbd37895ead3e3cd8836d2e437840ef
Matt Congdon <mattcong@users.noreply.github.com>
2025-05-18 21:19:21 +0100

    output file tracing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/calculate-alphabet': ['files/**/*'],
+    },
+  },
+}
 
 module.exports = nextConfig
